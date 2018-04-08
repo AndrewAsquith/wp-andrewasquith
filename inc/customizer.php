05a4422d66dba8cf123aff0276e88e90fb4bd882
Andrew Asquith <contact@andrewasquith.com>
2018-04-08 15:22:30 -0400

    Initial commit of underscores generated theme

---
+++ inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * andrewasquith Theme Customizer
+ *
+ * @package andrewasquith
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function andrewasquith_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'andrewasquith_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'andrewasquith_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'andrewasquith_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function andrewasquith_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function andrewasquith_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function andrewasquith_customize_preview_js() {
+	wp_enqueue_script( 'andrewasquith-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'andrewasquith_customize_preview_js' );
