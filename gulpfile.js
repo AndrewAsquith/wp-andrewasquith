6ec9ee3eed4be425d7fabb5299cccffc18ba9945
Andrew Asquith <contact@andrewasquith.com>
2018-04-14 16:47:00 -0400

    Added node, gulp and started gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
+
+var plumber = require('gulp-plumber');
+var sequence = require('gulp-sequence');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+
+var uglify = require('gulp-uglify');
+
+var conf = require('./gulpConfig.json');
+var paths = conf.paths;
+
+gulp.task('sass', function () {
+    var stream = gulp.src(paths.sass + '/*.scss')
+        .pipe(plumber({
+            errorHandler: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(sass({ errLogToConsole: true }))
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(sourcemaps.write(undefined, { sourceRoot: null }))
+        .pipe(gulp.dest(paths.css))
+    return stream;
+});
+
+gulp.task('minify-css', function () {
+    return gulp.src(paths.css + '/theme.css')
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(cleanCSS({ compatibility: '*' }))
+        .pipe(plumber({
+            errorHandler: function (err) {
+                console.log('Error: ' + err);
+                this.emit('end');
+            }
+        }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(paths.css));
+});
