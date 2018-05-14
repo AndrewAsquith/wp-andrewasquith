6db2e6a00a096cc6f0ea60d24a09a6a991d4a210
Andrew Asquith <contact@andrewasquith.com>
2018-05-13 21:49:50 -0400

    fix to custom header for blog page

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -28,12 +28,20 @@ function andrewasquith_custom_header_setup() {
 add_action( 'after_setup_theme', 'andrewasquith_custom_header_setup' );
 
 if (!function_exists('andrewasquith_get_header_image()')) {
+	/**
+	 * Helper function that returns the appropriate header image
+	 * 
+	 * Depends on whether the customizer option is set, and 
+	 * whether the page or post has a featured image
+	 */
 	function andrewasquith_get_header_image() {
 		$queried_post = get_queried_object_id();
 		
-		 if ((is_singular()) && ( get_theme_mod( 'featured_image_as_header' ) == 1 ) && (has_post_thumbnail($queried_post))) {
-			$attachment = wp_get_attachment_image_src( get_post_thumbnail_id( $queried_post ), 'full' );
-			return $attachment[0];
+		 if ((is_singular() || is_home() || is_front_page()) 
+			 && ( get_theme_mod( 'featured_image_as_header' ) == 1 ) 
+			 && (has_post_thumbnail($queried_post))) {
+				$attachment = wp_get_attachment_image_src( get_post_thumbnail_id( $queried_post ), 'full' );
+				return $attachment[0];
 		} else if (has_header_image()) {
 			return get_header_image();
 		} else {
