f1a33b6f6bd93bd0c7e3c81e68b5291e41d61776
Andrew Asquith <contact@andrewasquith.com>
2018-05-29 22:04:30 -0400

    added social links to footer

--- footer.php
+++ footer.php
@@ -10,11 +10,19 @@
  */
 
 ?>
-	<footer id="colophon" class="bg-dark site-footer mt-4">
-		<div class="text-center text-white pt-3">
+	<footer id="colophon" class="bg-dark site-footer pt-4 pb-2">
+		<div class="text-center">
+			<ul class="list-inline">
+			<li class="list-inline-item h2"><a href="https://goaa.ca/twitter" class="text-white"><span class="fab fa-twitter"></span></a></li>
+			<li class="list-inline-item h2"><a href="https://goaa.ca/linkedin" class="text-white"><span class="fab fa-linkedin"></span></a></li>
+			<li class="list-inline-item h2"><a href="https://goaa.ca/github" class="text-white"><span class="fab fa-github"></span></a></li>
+			</ul>
+		</div>
+		<div class="text-center">
+			<p class="text-white">
 			&copy;<a href="<?php echo esc_url( __( 'https://goaa.ca/me', 'andrewasquith' ) ); ?>" class="text-white">
 			<?php esc_html_e('Andrew Asquith', 'andrewasquith');?></a>
-			 | <a href="<?php echo esc_url(get_privacy_policy_url()); ?>" class="text-white">Privacy Policy</a>
+			 | <a href="<?php echo esc_url(get_privacy_policy_url()); ?>" class="text-white"><?php esc_html_e('Privacy Policy', 'andrewasquith');?></a></p>
 		</div>	
 	</footer><!-- #colophon -->
 </div> <!-- page -->
